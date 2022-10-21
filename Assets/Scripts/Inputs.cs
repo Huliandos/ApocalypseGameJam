18109f8221fdb2e729a1bfa1c242f65d5f3e2d1f
Julian Bechthold <julian.bechthold@gmail.com>
2022-10-21 15:21:06 +0200

    Added FPS controller. Started adding colliders to prefabs

---
+++ Assets/Scripts/Inputs.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inputs : MonoBehaviour
+{
+    public const string horizontal = "Horizontal";
+    public const string vertical = "Vertical";
+    public const string mouseX = "Mouse X";
+    public const string mouseY = "Mouse Y";
+    public const string sprint = "Sprint";
+    public const string interact = "Interact";
+}
